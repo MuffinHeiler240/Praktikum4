0fa59674a34c4fd77ab471c365ae596ce015dd2b
yuliia-zheliakova <yulia.zheliakova@gmail.com>
2024-12-16 16:51:23 +0100

    ♻️ Code refactoring

--- newton.c
+++ newton.c
@@ -11,69 +11,41 @@ double dF(double (*f)(double), double x) {
 }
 
 double newton(double (*f)(double), double x0) {
-    double x = x0;
-    double delta;
+    double x = x0, delta;
     iteNewton = 0;
     do {
-        double xn1 = x - f(x) / dF(f, x);
-        delta = (xn1 > x) ? (xn1 - x) : (x - xn1);
-        x = xn1;
-        iteNewton++;
-    } while (delta > EPS && iteNewton < MAXITERATIONS);
-    return (iteNewton == MAXITERATIONS ? NAN : x);
+        delta = -f(x) / dF(f, x);
+        x += delta;
+    } while (++iteNewton < MAXITERATIONS && fabs(delta) > EPS);
+    return (iteNewton == MAXITERATIONS) ? NAN : x;
 }
-
-/* Please check for errors and correct the following functions */
-/* Is this the correct way to implement the bisection method? */
 
 /* Find x with f(x)=0 and a <= x <= b using bisection method. */
 double bisection(double (*f)(double), double a, double b) {
-    double fa = f(a);
-    double fb = f(b);
     double c, fc;
-    if (fa * fb > 0) {
-        return NAN;
-    }
+    if (f(a) * f(b) > 0) return NAN;
     iteBisection = 0;
     do {
         c = (a + b) / 2;
-
-        fa = f(a);
         fc = f(c);
-
-        if (fa * fc < 0) {
-            b = c;
-        } else {
-            a = c;
-        }
+        if (f(a) * fc < 0) b = c;
+        else a = c;
         iteBisection++;
-    } while ((b - a > EPS) && (iteBisection < MAXITERATIONS));
-    return (iteBisection == MAXITERATIONS ? NAN : c);
+    } while ((b - a > EPS) && iteBisection < MAXITERATIONS);
+    return (iteBisection == MAXITERATIONS) ? NAN : c;
 }
 
-/* Please check for errors and correct the following functions */
-/* Is this the correct way to implement the regular falsi method? */
-
 /* Find x with f(x)=0 and a <= x <= b using regular falsi method. */
-double regualarfalsi(double (*f)(double), double a, double b) {
-    double fa = f(a);
-    double fb = f(b);
+double regulafalsi(double (*f)(double), double a, double b) {
     double c, fc;
-    if (fa * fb > 0) {
-        return NAN;
-    }
+    if (f(a) * f(b) > 0) return NAN;
     iteRegula = 0;
     do {
-        c = (a * fb - b * fa) / (fb - fa);
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a));
         fc = f(c);
-        if (fa * fc < 0) {
-            b = c;
-            fb = fc;
-        } else {
-            a = c;
-            fa = fc;
-        }
+        if (f(a) * fc < 0) b = c;
+        else a = c;
         iteRegula++;
     } while (fabs(fc) > EPS && iteRegula < MAXITERATIONS);
-    return (iteRegula == MAXITERATIONS ? NAN : c);
+    return (iteRegula == MAXITERATIONS) ? NAN : c;
 }
